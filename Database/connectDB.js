e40f84466694226620231292f8073dffab0cd9e6
Jayaraman-D <mr.jairam.d@gmail.com>
2025-10-14 23:52:32 +0530

    database set for render

--- Database/connectDB.js
+++ Database/connectDB.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose"
 const connectDB = async () => {
     try {
 
-       await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log('mongoDB Connected')
 
     } catch (error) {
